c4427e13df60eaadf35e1d408fc9142131486341
molgin <mollygingras@gmail.com>
2016-01-19 16:38:57 -0500

    organize some ice cream

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,33 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+-(NSArray *)namesForIceCream:(NSString *)iceCream{
+    NSMutableArray *names = [[NSMutableArray alloc] init];
+    NSDictionary *iceCreamByName = @{ @"Joe"    : @"Peanut Butter and Chocolate",
+                                      @"Tim"    : @"Natural Vanilla",
+                                      @"Sophie" : @"Mexican Chocolate",
+                                      @"Deniz"  : @"Natural Vanilla",
+                                      @"Tom"    : @"Mexican Chocolate",
+                                      @"Jim"    : @"Natural Vanilla",
+                                      @"Mark"   : @"Cookies 'n Cream" };
+    
+    for (NSString *name in [iceCreamByName allKeys]) {
+        if ([iceCreamByName[name] isEqualToString:iceCream]) {
+            [names addObject:name];
+        }
+    }
+    return names;
+}
+
+-(NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName{
+    NSMutableDictionary *iceCreamCounts = [[NSMutableDictionary alloc] init];
+    // what's the deal with this taking a passed in dictionary yet also relying on the hardcoded dictionary in namesForIceCream, it's weird
+    
+    for (NSString *iceCream in [iceCreamByName allValues]) {
+        iceCreamCounts[iceCream] = @([[self namesForIceCream:iceCream] count]);
+    }
+    
+    return iceCreamCounts;
+}
 
 @end
