cd094b6e9bd2ef6282a194ba0e2480c0727acb49
Raskovan <alexey.katalkin@gmail.com>
2016-07-26 19:18:56 -0400

    IceCream counted

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,45 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+- (NSArray *) namesForIceCream: (NSString *) iceCream {
+   
+    NSMutableArray *name = [[NSMutableArray alloc] init];
+    
+    NSDictionary *nameIceCream = @{@"Joe"       :   @"Peanut Butter and Chocolate",
+                                   @"Tim"       :   @"Natural Vanilla",
+                                   @"Sophie"    :   @"Mexican Chocolate",
+                                   @"Deniz"     :   @"Natural Vanilla",
+                                   @"Tom"       :   @"Mexican Chocolate",
+                                   @"Jim"       :   @"Natural Vanilla",
+                                   @"Mark"      :   @"Cookies 'n Cream"};
+    
+    for (NSString *key in nameIceCream) {
+        if ([nameIceCream[key] isEqualToString:iceCream])
+            [name addObject: key];
+        
+    }
+
+    return name;
+}
+
+
+
+- (NSDictionary *) countsOfIceCream: (NSDictionary *) iceCreamByName{
+    
+    FISAppDelegate *names = [[FISAppDelegate alloc] init];
+    NSMutableDictionary *iceCreamAndNames = [[NSMutableDictionary alloc] init];
+    
+    for (NSString *key in iceCreamByName) {
+    NSArray *name = [names namesForIceCream:iceCreamByName[key]];
+    NSUInteger x = [name count];
+    NSNumber *counts = @(x);
+    [iceCreamAndNames setObject:counts forKey:iceCreamByName[key]];
+    }
+    
+    
+    return iceCreamAndNames;
+    
+    }
+
 
 @end
